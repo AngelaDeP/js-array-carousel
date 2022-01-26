77db910b38e924ae32cd10bacdf85b6b8c079635
Angela De Pasquale <angeladepasquale_@libero.it>
2022-01-26 16:26:08 +0100

    Creazione esercizio

---
+++ main.js
@@ -0,0 +1,50 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+let mergedArray = [...items, ...title, ...text];
+
+//Sezione immagini grandi
+
+let slides ='';
+
+for (let i = 0; i < mergedArray.length; i++) {
+
+    slides +=   `<div class="item">
+                    <img src="${items[i]}">
+                    <div class="text-img">
+                        <h3>${title[i]}</h3>
+                        <p>${text[i]}</p>
+                    </div>
+                </div>`;
+}
+
+let carouselContainer = document.querySelector('.left-part');
+carouselContainer.innerHTML = slides;
+
+
+// Regole aggiunta classe
+let currentSlide = 0;
+
+let itemImages = document.getElementsByClassName('item');
+itemImages[currentSlide].classList.add('active');
